835fe015a1436d924378f7be170786461e2ed3a4
Michel Alves <michelalvs@yahoo.com.br>
2021-04-01 21:15:50 -0300

    CRUD Scaffolding done. Created Departments Controller and Views

---
+++ WebSalesMVC/Data/WebSalesMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebSalesMVC.Models;
+
+namespace WebSalesMVC.Data
+{
+    public class WebSalesMVCContext : DbContext
+    {
+        public WebSalesMVCContext (DbContextOptions<WebSalesMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebSalesMVC.Models.Department> Department { get; set; }
+    }
+}
--- WebSalesMVC/Startup.cs
+++ WebSalesMVC/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using WebSalesMVC.Data;
 
 namespace WebSalesMVC
 {
@@ -33,6 +35,9 @@ namespace WebSalesMVC
 
 
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+		    services.AddDbContext<WebSalesMVCContext>(options =>
+		            options.UseSqlServer(Configuration.GetConnectionString("WebSalesMVCContext")));
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
