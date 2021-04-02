e0cd7584fca40a632f0e205a03fb893ebc10d513
Michel Alves <michelalvs@yahoo.com.br>
2021-04-01 21:43:15 -0300

    Created First Migration.

--- WebSalesMVC/Startup.cs
+++ WebSalesMVC/Startup.cs
@@ -37,7 +37,8 @@ namespace WebSalesMVC
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
 		    services.AddDbContext<WebSalesMVCContext>(options =>
-		            options.UseSqlServer(Configuration.GetConnectionString("WebSalesMVCContext")));
+		            options.UseMySql(Configuration.GetConnectionString("WebSalesMVCContext"), builder =>
+					builder.MigrationsAssembly("WebSalesMVC")));
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
