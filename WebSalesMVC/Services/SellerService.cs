a4b03b52eea726f43b88b1034acbd3721f921ba6
Michel Alves <michelalvs@yahoo.com.br>
2021-04-09 23:39:44 -0300

    Created SellerService and Configured SellerController

---
+++ WebSalesMVC/Services/SellerService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebSalesMVC.Data;
+using WebSalesMVC.Models;
+
+namespace WebSalesMVC.Services
+{
+	public class SellerService
+	{
+		private readonly WebSalesMVCContext _context;
+
+		public SellerService(WebSalesMVCContext context)
+		{
+			_context = context;
+		}
+
+		public List<Seller> FindAll()
+		{
+			return _context.Seller.ToList();
+		}
+	}
+}
--- WebSalesMVC/Startup.cs
+++ WebSalesMVC/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using WebSalesMVC.Models;
 using WebSalesMVC.Data;
+using WebSalesMVC.Services;
 
 namespace WebSalesMVC
 {
@@ -42,6 +43,7 @@ namespace WebSalesMVC
 					builder.MigrationsAssembly("WebSalesMVC")));
 
 			services.AddScoped<SeedingService>();
+			services.AddScoped<SellerService>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
