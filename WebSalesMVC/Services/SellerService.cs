fabbc700b7e27672404972930eb0c7f6971ecb44
Michel Alves <michelalvs@yahoo.com.br>
2021-04-10 21:37:39 -0300

    Seller Department on details + Eager Loading

--- WebSalesMVC/Services/SellerService.cs
+++ WebSalesMVC/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using WebSalesMVC.Data;
 using WebSalesMVC.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebSalesMVC.Services
 {
@@ -29,7 +30,8 @@ namespace WebSalesMVC.Services
 
 		public Seller FindById(int id)
 		{
-			return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+			return _context.Seller.Include( obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
+			
 		}
 
 		public void Remove (int id)
