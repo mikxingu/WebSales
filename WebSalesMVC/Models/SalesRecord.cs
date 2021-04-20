cc7c9237638efb99dff3f7aa2509dd76a7a7ea00
Michel Alves <michelalvs@gmail.com>
2021-04-20 20:51:43 -0300

    Implemented Grouping Search

--- WebSalesMVC/Models/SalesRecord.cs
+++ WebSalesMVC/Models/SalesRecord.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using WebSalesMVC.Models.Enums;
 
 namespace WebSalesMVC.Models
@@ -6,8 +7,11 @@ namespace WebSalesMVC.Models
 	public class SalesRecord
 	{
 		public int Id { get; set; }
+
+		[DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
 		public DateTime Date { get; set; }
 
+		[DisplayFormat(DataFormatString = "{0:F2}")]
 		public double Amount { get; set; }
 
 		public SaleStatus Status { get; set; }
@@ -19,6 +23,7 @@ namespace WebSalesMVC.Models
 
 		}
 
+		
 		public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
 		{
 			Id = id;
